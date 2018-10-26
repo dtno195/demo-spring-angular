58c2c8ab3a7830ee3634fa765ba579fbf88e0899
nws-ducnt <ducnt@newwave.vn>
2018-10-26 21:18:06 +0700

    comiit

---
+++ src/main/java/com/sd/maon/DemoSpringBootRestAngularApplication.java
@@ -0,0 +1,12 @@
+package com.sd.maon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootRestAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootRestAngularApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sd/maon/DemoSpringBootRestAngularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sd.maon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringBootRestAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
